4704eb4018e780ce4b07286d2960b5f2876b6832
nicolaasuni <info@tecnick.com>
2017-02-12 15:50:41 +0000

    Update dependencies

--- test/TcpdfTest.php
+++ test/TcpdfTest.php
@@ -15,6 +15,8 @@
 
 namespace Test;
 
+use PHPUnit\Framework\TestCase;
+
 /**
  * Tcpdf Pdf class test
  *
@@ -26,7 +28,7 @@ namespace Test;
  * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
  * @link        https://github.com/tecnickcom/tc-lib-pdf
  */
-class TcpdfTest extends \PHPUnit_Framework_TestCase
+class TcpdfTest extends TestCase
 {
     protected $obj = null;
 
