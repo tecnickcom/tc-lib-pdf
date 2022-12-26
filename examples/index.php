32ac0cdbfc2382582087eff8ec114e91f54f7f77
nicolaasuni <info@tecnick.com>
2022-12-26 14:14:05 +0000

    Add more graphic examples

--- examples/index.php
+++ examples/index.php
@@ -475,6 +475,7 @@ $pdf->page->addContent($crpmrk9);
 
 // Registration Marks
 
+
 $regmrk1 = $pdf->graph->getRegistrationMark(40, 60, 5, false);
 $pdf->page->addContent($regmrk1);
 
@@ -570,6 +571,22 @@ $color_custom_yellow =  new \Com\Tecnick\Color\Model\Cmyk(
 );
 $pdf->color->addSpotColor('Custom Yellow', $color_custom_yellow);
 
+
+// Set Style
+$style8 = array(
+    'lineWidth' => 0,
+    'lineCap'   => 0,
+    'lineJoin'  => 0,
+    'dashArray' => array(),
+    'dashPhase' => 0,
+    'lineColor' => 'white',
+    'fillColor' => 'white',
+);
+
+
+$pdf->page->addContent($pdf->graph->getStyleCmd($style8));
+
+
 // Color Registration Bars with spot colors
 
 $colregspot = $pdf->graph->getColorRegistrationBar(30, 150, 100, 10, true,
@@ -589,9 +606,110 @@ array(
 $pdf->page->addContent($colregspot);
 
 
+$colreg1 = $pdf->graph->getColorRegistrationBar(50, 70, 40, 40, false);
+$pdf->page->addContent($colreg1);
 
-//$colreg1 = $pdf->graph->getColorRegistrationBar(50, 70, 40, 40, true);
-//$pdf->page->addContent($colreg1);
+$colreg2 = $pdf->graph->getColorRegistrationBar(90, 70, 40, 40, true);
+$pdf->page->addContent($colreg2);
+
+$barcols = array('black', 'white', 'red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'gray', 'black');
+
+$colreg3 = $pdf->graph->getColorRegistrationBar(50, 115, 80, 5, true, $barcols);
+$pdf->page->addContent($colreg3);
+
+$colreg4 = $pdf->graph->getColorRegistrationBar(135, 70, 5, 50, false, $barcols);
+$pdf->page->addContent($colreg4);
+
+
+// ----------
+// Add sixth page
+
+$page06 = $pdf->page->add();
+
+$pdf->graph->setPageWidth($page06['width']);
+$pdf->graph->setPageHeight($page06['height']);
+
+
+// Linear gradient
+$lingrad = $pdf->graph->getLinearGradient(20, 45, 80, 80, 'red', 'blue', array(0,0,1,0));
+$pdf->page->addContent($lingrad);
+
+// Radial Gradient
+$radgrad = $pdf->graph->getRadialGradient(110, 45, 80, 80, 'white', 'black', array(0.5, 0.5, 1, 1, 1.2));
+$pdf->page->addContent($radgrad);
+
+
+// CoonsPatchMesh
+$coonspatchmesh1 = $pdf->graph->getCoonsPatchMesh(20, 155, 80, 80);
+$pdf->page->addContent($coonspatchmesh1);
+
+
+// set the coordinates for the cubic Bézier points x1,y1 ... x12, y12 of the patch
+$coords = array(
+	0.00,0.00, 0.33,0.20,             //lower left
+	0.67,0.00, 1.00,0.00, 0.80,0.33,  //lower right
+	0.80,0.67, 1.00,1.00, 0.67,0.80,  //upper right
+	0.33,1.00, 0.00,1.00, 0.20,0.67,  //upper left
+	0.00,0.33);                       //lower left
+
+// paint a coons patch gradient with the above coordinates
+$coonspatchmesh2 = $pdf->graph->getCoonsPatchMesh(110, 155, 80, 80, 'yellow', 'blue', 'green', 'red', $coords, 0, 1);
+$pdf->page->addContent($coonspatchmesh2);
+
+
+// ----------
+// Add seventh page
+
+$page07 = $pdf->page->add();
+
+$pdf->graph->setPageWidth($page07['width']);
+$pdf->graph->setPageHeight($page07['height']);
+
+// first patch: f = 0
+$patch_array[0]['f'] = 0;
+$patch_array[0]['points'] = array(
+	0.00,0.00, 0.33,0.00,
+	0.67,0.00, 1.00,0.00, 1.00,0.33,
+	0.8,0.67, 1.00,1.00, 0.67,0.8,
+	0.33,1.80, 0.00,1.00, 0.00,0.67,
+	0.00,0.33);
+$patch_array[0]['colors'][0] = array('red' => 1, 'green' => 1, 'blue' => 0, 'alpha' => 1);
+$patch_array[0]['colors'][1] = array('red' => 1, 'green' => 1, 'blue' => 1, 'alpha' => 1);
+$patch_array[0]['colors'][2] = array('red' => 0, 'green' => 1, 'blue' => 0, 'alpha' => 1);
+$patch_array[0]['colors'][3] = array('red' => 1, 'green' => 0, 'blue' => 0, 'alpha' => 1);
+
+// second patch - above the other: f = 2
+$patch_array[1]['f'] = 2;
+$patch_array[1]['points'] = array(
+	0.00,1.33,
+	0.00,1.67, 0.00,2.00, 0.33,2.00,
+	0.67,2.00, 1.00,2.00, 1.00,1.67,
+	1.5,1.33);
+$patch_array[1]['colors'][0] = array('red' => 0, 'green' => 0, 'blue' => 0, 'alpha' => 1);
+$patch_array[1]['colors'][1] = array('red' => 1, 'green' => 0, 'blue' => 1, 'alpha' => 1);
+
+// third patch - right of the above: f = 3
+$patch_array[2]['f'] = 3;
+$patch_array[2]['points'] = array(
+	1.33,0.80,
+	1.67,1.50, 2.00,1.00, 2.00,1.33,
+	2.00,1.67, 2.00,2.00, 1.67,2.00,
+	1.33,2.00);
+$patch_array[2]['colors'][0] = array('red' => 0, 'green' => 1, 'blue' => 1, 'alpha' => 1);
+$patch_array[2]['colors'][1] = array('red' => 0, 'green' => 0, 'blue' => 0, 'alpha' => 1);
+
+// fourth patch - below the above, which means left(?) of the above: f = 1
+$patch_array[3]['f'] = 1;
+$patch_array[3]['points'] = array(
+	2.00,0.67,
+	2.00,0.33, 2.00,0.00, 1.67,0.00,
+	1.33,0.00, 1.00,0.00, 1.00,0.33,
+	0.8,0.67);
+$patch_array[3]['colors'][0] = array('red' => 0, 'green' => 0, 'blue' => 0, 'alpha' => 1);
+$patch_array[3]['colors'][1] = array('red' => 0, 'green' => 0, 'blue' => 1, 'alpha' => 1);
+
+$coonspatchmesh3 = $pdf->graph->getCoonsPatchMesh(0, 0, 210, 297, '', '', '', '', $patch_array, 0, 2);
+$pdf->page->addContent($coonspatchmesh3);
 
 
 // ----------
---
+++ examples/test.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * index.php
+ *
+ * @since       2017-05-08
+ * @category    Library
+ * @package     Pdf
+ * @author      Nicola Asuni <info@tecnick.com>
+ * @copyright   2002-2017 Nicola Asuni - Tecnick.com LTD
+ * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
+ * @link        https://github.com/tecnickcom/tc-lib-pdf
+ *
+ * This file is part of tc-lib-pdf software library.
+ */
+
+// NOTE: run make deps fonts in the project root to generate the dependencies and example fonts.
+
+// autoloader when using Composer
+require ('../vendor/autoload.php');
+
+use \Com\Tecnick\Color\Model\Cmyk as ColorCMYK;
+use \Com\Tecnick\Color\Model\Gray as ColorGray;
+use \Com\Tecnick\Color\Model\Hsl as ColorHSL;
+use \Com\Tecnick\Color\Model\Rgb as ColorRGB;
+
+define('OUTPUT_FILE', '../target/test.pdf');
+
+// define fonts directory
+define('K_PATH_FONTS', '../vendor/tecnickcom/tc-lib-pdf-font/target/fonts/core/');
+
+// autoloader when using RPM or DEB package installation
+//require ('/usr/share/php/Com/Tecnick/Pdf/autoload.php');
+
+// main TCPDF object
+$pdf = new \Com\Tecnick\Pdf\Tcpdf('mm', true, false, false, '');
+
+// ----------
+// Set Metadata
+
+$pdf->setCreator('tc-lib-pdf');
+$pdf->setAuthor('John Doe');
+$pdf->setSubject('tc-lib-pdf example');
+$pdf->setTitle('Example');
+$pdf->setKeywords('TCPDF','tc-lib-pdf','example');
+
+
+$page05 = $pdf->page->add();
+
+$pdf->graph->setPageWidth($page05['width']);
+$pdf->graph->setPageHeight($page05['height']);
+
+
+$coonspatchmesh = $pdf->graph->getCoonsPatchMesh(20, 155, 80, 80);
+$pdf->page->addContent($coonspatchmesh);
+
+
+// ----------
+
+
+// PDF document as string
+$doc = $pdf->getOutPDFString();
+
+// Debug document output:
+//var_export($doc);
+
+// Save the PDF document as a file
+file_put_contents(OUTPUT_FILE, $doc);
+
+echo 'OK: '.OUTPUT_FILE;
