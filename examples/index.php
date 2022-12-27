d57890b79c43052c6eddb11139551dafe000cb81
nicolaasuni <info@tecnick.com>
2022-12-27 11:41:53 +0000

    Color transformation examples

--- examples/index.php
+++ examples/index.php
@@ -722,12 +722,32 @@ $pdf->graph->setPageHeight($page08['height']);
 // Geometric Transformations
 
 
+$rect_style =  array(
+    'lineWidth' => 0.3,
+    'lineCap'   => 'butt',
+    'lineJoin'  => 'miter',
+    'dashArray' => array(),
+    'dashPhase' => 0,
+    'lineColor' => 'black',
+    'fillColor' => 'black',
+);
+
+$transform_style =  array(
+    'lineWidth' => 0.3,
+    'lineCap'   => 'butt',
+    'lineJoin'  => 'miter',
+    'dashArray' => array(),
+    'dashPhase' => 0,
+    'lineColor' => 'red',
+    'fillColor' => 'red',
+);
+
 // Scaling
-$t1 = $pdf->graph->getBasicRect(50, 70, 40, 10, 'D');
+$t1 = $pdf->graph->getBasicRect(50, 70, 40, 10, 'D', $rect_style);
 $t1 .= $pdf->graph->getStartTransform();
 // Scale by 150% centered by (50,80) which is the lower left corner of the rectangle
 $t1 .= $pdf->graph->getScaling(1.5, 1.5, 50, 80);
-$t1 .= $pdf->graph->getBasicRect(50, 70, 40, 10, 'D');
+$t1 .= $pdf->graph->getBasicRect(50, 70, 40, 10, 'D', $transform_style);
 $t1 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t1);
 
@@ -737,7 +757,7 @@ $t2 = $pdf->graph->getBasicRect(125, 70, 40, 10, 'D');
 $t2 .= $pdf->graph->getStartTransform();
 // Translate 7 to the right, 5 to the bottom
 $t2 .= $pdf->graph->getTranslation(7, 5);
-$t2 .= $pdf->graph->getBasicRect(125, 70, 40, 10, 'D');
+$t2 .= $pdf->graph->getBasicRect(125, 70, 40, 10, 'D', $transform_style);
 $t2 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t2);
 
@@ -747,7 +767,7 @@ $t3 = $pdf->graph->getBasicRect(70, 100, 40, 10, 'D');
 $t3 .= $pdf->graph->getStartTransform();
 // Rotate 20 degrees counter-clockwise centered by (70,110) which is the lower left corner of the rectangle
 $t3 .= $pdf->graph->getRotation(20, 70, 110);
-$t3 .= $pdf->graph->getBasicRect(70, 100, 40, 10, 'D');
+$t3 .= $pdf->graph->getBasicRect(70, 100, 40, 10, 'D', $transform_style);
 $t3 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t3);
 
@@ -757,7 +777,7 @@ $t4 = $pdf->graph->getBasicRect(125, 100, 40, 10, 'D');
 $t4 .= $pdf->graph->getStartTransform();
 // skew 30 degrees along the x-axis centered by (125,110) which is the lower left corner of the rectangle
 $t4 .= $pdf->graph->getSkewing(30, 0, 125, 110);
-$t4 .= $pdf->graph->getBasicRect(125, 100, 40, 10, 'D');
+$t4 .= $pdf->graph->getBasicRect(125, 100, 40, 10, 'D', $transform_style);
 $t4 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t4);
 
@@ -767,7 +787,7 @@ $t5 = $pdf->graph->getBasicRect(70, 130, 40, 10, 'D');
 $t5 .= $pdf->graph->getStartTransform();
 // mirror horizontally with axis of reflection at x-position 70 (left side of the rectangle)
 $t5 .= $pdf->graph->getHorizMirroring(70);
-$t5 .= $pdf->graph->getBasicRect(70, 130, 40, 10, 'D');
+$t5 .= $pdf->graph->getBasicRect(70, 130, 40, 10, 'D', $transform_style);
 $t5 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t5);
 
@@ -777,7 +797,7 @@ $t6 = $pdf->graph->getBasicRect(125, 130, 40, 10, 'D');
 $t6 .= $pdf->graph->getStartTransform();
 // mirror vertically with axis of reflection at y-position 140 (bottom side of the rectangle)
 $t6 .= $pdf->graph->getVertMirroring(140);
-$t6 .= $pdf->graph->getBasicRect(125, 130, 40, 10, 'D');
+$t6 .= $pdf->graph->getBasicRect(125, 130, 40, 10, 'D', $transform_style);
 $t6 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t6);
 
@@ -787,7 +807,7 @@ $t7 = $pdf->graph->getBasicRect(70, 160, 40, 10, 'D');
 $t7 .= $pdf->graph->getStartTransform();
 // point reflection at the lower left point of rectangle
 $t7 .= $pdf->graph->getPointMirroring(70,170);
-$t7 .= $pdf->graph->getBasicRect(70, 160, 40, 10, 'D');
+$t7 .= $pdf->graph->getBasicRect(70, 160, 40, 10, 'D', $transform_style);
 $t7 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t7);
 
@@ -797,17 +817,17 @@ $px=120;
 $py=170;
 
 // just for visualisation: the straight line to mirror against
-$t8 = $pdf->graph->getLine($px-1,$py-1,$px+1,$py+1);
-$t8 .= $pdf->graph->getLine($px-1,$py+1,$px+1,$py-1);
+$t8 = $pdf->graph->getLine($px-1,$py-1,$px+1,$py+1, array('lineColor' => 'green'));
+$t8 .= $pdf->graph->getLine($px-1,$py+1,$px+1,$py-1, array('lineColor' => 'green'));
 $t8 .= $pdf->graph->getStartTransform();
 $t8 .= $pdf->graph->getRotation($angle, $px, $py);
-$t8 .= $pdf->graph->getLine($px-5, $py, $px+60, $py);
+$t8 .= $pdf->graph->getLine($px-5, $py, $px+60, $py, array('lineColor' => 'green'));
 $t8 .= $pdf->graph->getStopTransform();
-$t8 .= $pdf->graph->getBasicRect(125, 160, 40, 10, 'D');
+$t8 .= $pdf->graph->getBasicRect(125, 160, 40, 10, 'D', $rect_style);
 $t8 .= $pdf->graph->getStartTransform();
 // mirror against the straight line
 $t8 .= $pdf->graph->getReflection($angle, $px, $py);
-$t8 .= $pdf->graph->getBasicRect(125, 160, 40, 10, 'D');
+$t8 .= $pdf->graph->getBasicRect(125, 160, 40, 10, 'D', $transform_style);
 $t8 .= $pdf->graph->getStopTransform();
 $pdf->page->addContent($t8);
 
