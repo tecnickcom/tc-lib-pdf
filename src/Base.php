8773ec952bf8fca025a38a48f6afa1aac64f7b1f
nicolaasuni <info@tecnick.com>
2024-08-28 12:38:04 +0100

    Add class placeholders

--- src/Base.php
+++ src/Base.php
@@ -156,7 +156,7 @@ abstract class Base
     /**
      * TCPDF version.
      */
-    protected string $version = '8.0.77';
+    protected string $version = '8.0.78';
 
     /**
      * Time is seconds since EPOCH when the document was created.
---
+++ src/CSS.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * CSS.php
+ *
+ * @since     2002-08-03
+ * @category  Library
+ * @package   Pdf
+ * @author    Nicola Asuni <info@tecnick.com>
+ * @copyright 2002-2024 Nicola Asuni - Tecnick.com LTD
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
+ * @link      https://github.com/tecnickcom/tc-lib-pdf
+ *
+ * This file is part of tc-lib-pdf software library.
+ */
+
+namespace Com\Tecnick\Pdf;
+
+use Com\Tecnick\Pdf\Exception as PdfException;
+
+/**
+ * Com\Tecnick\Pdf\CSS
+ *
+ * CSS PDF class
+ *
+ * @since     2002-08-03
+ * @category  Library
+ * @package   Pdf
+ * @author    Nicola Asuni <info@tecnick.com>
+ * @copyright 2002-2024 Nicola Asuni - Tecnick.com LTD
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
+ * @link      https://github.com/tecnickcom/tc-lib-pdf
+ */
+abstract class CSS extends \Com\Tecnick\Pdf\SVG
+{
+}
