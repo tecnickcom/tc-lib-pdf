b63a499dfeca9e41cfe1ed2b801ef74ba8b23e33
nicolaasuni <info@tecnick.com>
2024-10-26 13:38:44 +0100

    Update dependencies

--- src/Base.php
+++ src/Base.php
@@ -176,7 +176,7 @@ abstract class Base
     /**
      * TCPDF version.
      */
-    protected string $version = '8.0.80';
+    protected string $version = '8.0.81';
 
     /**
      * Time is seconds since EPOCH when the document was created.
