bb83ca965bd2b5310688ac2602591c2f907dda62
Nicola Asuni <info@tecnick.com>
2025-12-04 17:14:51 +0000

    fix version

--- src/Base.php
+++ src/Base.php
@@ -180,7 +180,7 @@ abstract class Base
     /**
      * TCPDF version.
      */
-    protected string $version = '8.3.0';
+    protected string $version = '8.2.3';
 
     /**
      * Time is seconds since EPOCH when the document was created.
