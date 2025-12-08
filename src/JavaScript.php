3348a40aabb2860f778456aa935259cdc50081b3
Nicola Asuni <info@tecnick.com>
2025-12-08 12:21:19 +0000

    move attributes

--- src/JavaScript.php
+++ src/JavaScript.php
@@ -36,4 +36,60 @@ use Com\Tecnick\Pdf\Exception as PdfException;
 abstract class JavaScript extends \Com\Tecnick\Pdf\CSS
 {
     //@TODO
+
+    /**
+     * Fonts used in annotations.
+     *
+     * @var array<string, int>
+     */
+    protected array $annotation_fonts = [];
+
+    /**
+     * Destinations.
+     *
+     * @var array<string, array{
+     *          'p': int,
+     *          'x': float,
+     *          'y': float,
+     *      }>
+     */
+    protected array $dests = [];
+
+    /**
+     * Links.
+     *
+     * @var array<string, array{
+     *          'p': int,
+     *          'y': float,
+     *      }>
+     */
+    protected array $links = [];
+
+    /**
+     * Radio Button Groups.
+     *
+     * @var array<string, array{
+     *         'n': int,
+     *         '#readonly#': bool,
+     *         'kid'?: int,
+     *         'def': string,
+     *      }>
+     */
+    protected array $radiobuttonGroups = [];
+
+    /**
+     * Javascript block to add.
+     */
+    protected string $javascript = '';
+
+    /**
+     * Javascript objects.
+     *
+     * @var array<int, array{
+     *          'n': int,
+     *          'js': string,
+     *          'onload': bool,
+     *      }>
+     */
+    protected array $jsobjects = [];
 }
--- src/Output.php
+++ src/Output.php
@@ -469,62 +469,6 @@ abstract class Output extends \Com\Tecnick\Pdf\MetaInfo
     protected array $lang = [];
 
     /**
-     * Fonts used in annotations.
-     *
-     * @var array<string, int>
-     */
-    protected array $annotation_fonts = [];
-
-    /**
-     * Destinations.
-     *
-     * @var array<string, array{
-     *          'p': int,
-     *          'x': float,
-     *          'y': float,
-     *      }>
-     */
-    protected array $dests = [];
-
-    /**
-     * Links.
-     *
-     * @var array<string, array{
-     *          'p': int,
-     *          'y': float,
-     *      }>
-     */
-    protected array $links = [];
-
-    /**
-     * Radio Button Groups.
-     *
-     * @var array<string, array{
-     *         'n': int,
-     *         '#readonly#': bool,
-     *         'kid'?: int,
-     *         'def': string,
-     *      }>
-     */
-    protected array $radiobuttonGroups = [];
-
-    /**
-     * Javascript block to add.
-     */
-    protected string $javascript = '';
-
-    /**
-     * Javascript objects.
-     *
-     * @var array<int, array{
-     *          'n': int,
-     *          'js': string,
-     *          'onload': bool,
-     *      }>
-     */
-    protected array $jsobjects = [];
-
-    /**
      * Returns the RAW PDF string.
      */
     public function getOutPDFString(): string
