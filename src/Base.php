8ad3a9aeb98e11c3f575f6ef5a826a0192bee372
Nicola Asuni <info@tecnick.com>
2026-02-25 10:02:38 +0000

    bump version

--- src/Base.php
+++ src/Base.php
@@ -180,7 +180,7 @@ abstract class Base
     /**
      * TCPDF version.
      */
-    protected string $version = '8.4.12';
+    protected string $version = '8.4.13';
 
     /**
      * Time is seconds since EPOCH when the document was created.
